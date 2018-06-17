b3bd9d2ccd08ca41e38208405cfcda699630d4b5
nolanvo <nolan05081994@gmail.com>
2018-06-17 09:39:52 -0500

    change move_list representation from dictionary to list of list

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,29 +1,34 @@
 def create_move_list(side):
-	for row_index in range(side):
-		for col_index in range(side):
-			move_list[str(row_index) + str(col_index)] = ' '
+	for row in range(side):
+		move_list.append([])
+		for col in range(side):
+			move_list[row].append(' ')
 
 def createBoard(side, move_list):
-	for row_index in range(side):
-		for col_index in range(side):
-			print(move_list[str(row_index) + str(col_index)]+'|', end = '')
+	for row in range(side):
+		for col in range(side):
+		
+			print(move_list[row][col]+'|', end = '')
 		print('\n')
 
-def makeAMove(move):
-	move_list[move] = 'x'
+def makeAMove(row, col):
+	move_list[row][col] = 'x'
 	createBoard(side, move_list)
+
 
 #def checkOver(move_list)
 
-side = int(input('How big do you want the board to be: '))
-move_list = {}
+side = int(input('How big do you want the board to be: ')) #3
+move_list = []
 create_move_list(side)
 createBoard(side, move_list)
 
 
 for i in range(3):
-	move = input('Where do you want to put your sign: ')
-	makeAMove(move)
+	move = input('Where do you want to put your sign: ').split()
+	row = int(move[0])
+	col = int(move[1])
+	makeAMove(row, col)
 	print(move_list)
 
 
