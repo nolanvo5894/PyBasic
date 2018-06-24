d1401246101cfe8f3825530abcc39b5852f88b35
nolanvo <nolan05081994@gmail.com>
2018-06-23 22:34:35 -0500

    made the game for one dealer and one player

--- BJ.py
+++ BJ.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 class Deck:
 	cardList = list(range(1,10))*4
 	def __init__ (self, cardNumber):
@@ -17,17 +15,11 @@ class Deck:
 	def showCardList(self):
 	 	print (self.cardList)
 
-myDeck = Deck(52)
-
-
-
-# need to add the secret card later
 class Dealer():
 	def __init__ (self):
 		self.score = 0
 		self.hand = []
-	#card = myDeck.throwOut()
-	#need to take in twice at the start of the game
+	
 
 	def takeIn(self, card): 
 		self.hand.append(card)
@@ -39,19 +31,121 @@ class Dealer():
 	def checkBJ(self):
 		return self.score == 21
 
-	def checkScore(self):
-		return self.score
+	
+class Player():
+	def __init__ (self, fund):
+		self.score = 0
+		self.hand = []
+		self.fund = fund
+
+	def takeIn(self, card): 
+		self.hand.append(card)
+		self.score += card 
+
+	def checkBurst(self):
+		return self.score > 21
+
+	def checkBJ(self):
+		return self.score == 21
+
+	
+def game():
+
+	if myPlayer.checkBJ():
+		print('You won with a BJ')
+		return 
+	else:
+
+
+	# Ask the player whether to hit or stand
+
+
+		playerChoice = input('Press h for hit and s for stand: ')
+
+
+		# If player choose 'Hit', deal cards until:
+			# Player chooses 'Stand'.
+			# Player gets BJ.
+			# Player bursts.
+
+		if playerChoice == 'h':
+			while playerChoice != 's' and myPlayer.checkBJ() != True and myPlayer.checkBurst() != True:
+				myPlayer.takeIn(myDeck.throwOut())
+				if myPlayer.checkBJ():
+					print('You won with a BJ')
+					myPlayer.fund += bet
+					return 
+				if myPlayer.checkBurst():
+					print('You burst. Try again')
+					myPlayer.fund -= bet
+					return 
+				print(f'Your current score is {myPlayer.score}')
+				playerChoice = input('Press H for Hit and S for Stand: ')
+				if playerChoice == 's':
+					dealerTurn()
+
+		else:
+			dealerTurn()
 
 
 
-myDealer = Dealer()
-myDealer.takeIn(myDeck.throwOut())
-myDeck.showCardList()
+def dealerTurn():
+	print(myDealer.hand)
+	while myDealer.score < 17:
+		myDealer.takeIn(myDeck.throwOut())
+	print(f"Dealer's score is {myDealer.score}")
+	if myDealer.checkBJ():
+		print('Dealer won with BJ')
+		myPlayer.fund -= bet
+	elif myDealer.checkBurst():
+		print('Dealer burst! Player won!')
+		myPlayer.fund += bet
+	else:
+		if myPlayer.score == myDealer.score:
+			print('Tie!')
+		elif myPlayer.score > myDealer.score:
+			print('Player won!')
+			myPlayer.fund += bet
+		else:
+			print('Dealer won!')
+			myPlayer.fund -= bet
 
-print(myDealer.hand)
-print(myDealer.checkBurst())
-print(myDealer.checkBJ())
-print(myDealer.checkScore())
+
+
+if __name__ == '__main__':
+
+	play = 'y'
+	fund = float(input('How much money do you have? '))
+	myPlayer = Player(fund)
+
+
+	while play == 'y':
+		myDeck = Deck(52)
+		myPlayer.score = 0
+		bet = float(input('How much money do you want to bet? '))
+		myDealer = Dealer()
+
+		# Start the game by dealing cards to the dealer and the player
+
+		myPlayer.takeIn(myDeck.throwOut())
+		myPlayer.takeIn(myDeck.throwOut())
+		print(f'Your score is {myPlayer.score}')
+
+		myDealer.takeIn(myDeck.throwOut())
+		myDealer.takeIn(myDeck.throwOut())
+
+		game()
+		print(f'You have {myPlayer.fund} in your fund')
+		play = input('Do you want to keep playing? Press y for yes and n for no: ')
+
+
+
+	
+
+
+
+
+
 
 
 
