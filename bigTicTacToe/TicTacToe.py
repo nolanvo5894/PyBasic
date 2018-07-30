70d759fe4c8a68634b3e202a14da8bb3504b2e95
nolanvo <nolan05081994@gmail.com>
2018-07-29 23:25:38 -0500

    add some comments for bigTacTacToe

--- bigTicTacToe/TicTacToe.py
+++ bigTicTacToe/TicTacToe.py
@@ -1,45 +1,35 @@
-
-
-
 from checkOver import checkOver
 from boardSetUp.boardSetUp import createMoveList, createBoard, makeAMove, signSelection
-	
+
 
 
 
 
 #MAIN PROGRAM
-
-side = int(input('How big do you want the board to be ?: '))
-num_players = int(input('How many players are in the games ?: '))
-streak = int(input('How long should the winning streak be ?: ')) 
-sign_list = []
-signSelection(sign_list, num_players)
-move_list = []
-createMoveList(side, move_list)
-createBoard(side, move_list)
-
-
-turn = 0
-check = False
-
-while check == False:
-	for i in range (1, num_players + 1):
-		if turn%num_players == (i-1):
-			sign = sign_list[i-1]
-			move = input('Where do you want to put your sign, player {}: '.format(i)).split()
-			row = int(move[0])
-			col = int(move[1])
-			makeAMove(row, col, sign, side, move_list)
-			if checkOver(move_list, sign, side, streak):
-				print('Winner is player {} !'.format(i))
-				check = True
-				break
-	turn += 1
+if __name__ == '__main__':
+	side = int(input('How big do you want the board to be ?: '))
+	num_players = int(input('How many players are in the games ?: '))
+	streak = int(input('How long should the winning streak be ?: '))
+	sign_list = []
+	signSelection(sign_list, num_players)
+	move_list = []
+	createMoveList(side, move_list)
+	createBoard(side, move_list)
 
 
+	turn = 0
+	check = False
 
-
-	
-
-
+	while check == False:
+		for i in range (1, num_players + 1):
+			if turn%num_players == (i-1):
+				sign = sign_list[i-1]
+				move = input('Where do you want to put your sign, player {}: '.format(i)).split()
+				row = int(move[0])
+				col = int(move[1])
+				makeAMove(row, col, sign, side, move_list)
+				if checkOver(move_list, sign, side, streak):
+					print('Winner is player {} !'.format(i))
+					check = True
+					break
+		turn += 1
--- bigTicTacToe/boardSetUp/boardSetUp.py
+++ bigTicTacToe/boardSetUp/boardSetUp.py
@@ -1,19 +1,26 @@
-def createMoveList(side, move_list):
-	for row in range(side):
-		move_list.append([])
-		for col in range(side):
-			move_list[row].append(' ')
+#functions to create the board and print out the board, let user select the sign they want to use in the game,
+#let users make a move, and then store the move in a list.
 
+#function to let user select a sign for the game, for example 'x' or 'o' or 's'
+def signSelection(sign_list, num_players):
+	for i in range (1, num_players + 1):
+		sign_list.append(input('What is the sign for player {}: '.format(i)))
+
+#function to create a board of size sidexside and a list of all cells with row index and column index (a list of list actually)
 def createBoard(side, move_list):
 	for row in range(side):
 		for col in range(side):
 			print(move_list[row][col]+'|', end = '')
 		print('\n')
 
+#function to create a list of list to store moves made by players.
+def createMoveList(side, move_list):
+	for row in range(side):
+		move_list.append([])
+		for col in range(side):
+			move_list[row].append(' ')
+
+#function to let user make a move by putting their in a selected cell.
 def makeAMove(row, col, sign, side, move_list):
 	move_list[row][col] = sign
 	createBoard(side, move_list)
-
-def signSelection(sign_list, num_players):
-	for i in range (1, num_players + 1):
-		sign_list.append(input('What is the sign for player {}: '.format(i)))
