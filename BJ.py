70f7e71728d117ccad2b472721f4a13282eb4fca
nolanvo <nolan05081994@gmail.com>
2018-07-30 00:09:58 -0500

    added some comments for BJ.py

--- BJ.py
+++ BJ.py
@@ -1,5 +1,21 @@
+# this is the program for a simplified game of Black Jack between 1 Dealer and 1 Player.
+# both the player and the dealer tries to increase their points up to but not higher than 21 by drawing cards from the Deck.
+# number cards carry the same amount of points as their numbers. All face cards carry 10 points and are treated the same. The 'A' card can carry either 1 or 11 points, whichever is better for its drawer.
+# the Deck's original card list is made up of 52 numbers ranging from 1 to 10, each appears 4 times, and 4 'A's.
+# whoever gets higher than 21 points bursts and lose the game.
+# the one who get exactly 21 points first win with a Black Jack.
+# the one who burst first loses.
+# if no one bursts, the one with the higher number of points win.
+# the player can choose when to stop taking in card in his/her turn.
+# the dealer have to keep taking in card until its point is >= 17.
+
 import random
 
+# create a Deck class with:
+	# attribute 'cardList' that keeps track of the list of cards in the Deck.
+	# attribute 'cardNumber' that keeps track of the number of cards in the Deck.
+	# method throwOut to deal the card to either the Dealer or the Player.
+	# method showCardList that prints out the list of all cards in the Deck.
 class Deck:
 	cardList = list(range(1,11))*4
 	for i in range(4):
@@ -19,17 +35,23 @@ class Deck:
 	def showCardList(self):
 	 	print (self.cardList)
 
+# create a Dealer class with:
+	# attribute 'score' which keeps track of the dealer's number of points.
+	# attribute 'hand' which keeps track of the list of cards on the dealer's hand.
+	# method takeIn for taking in a card from the Deck.
+	# method checkBurst to see if the Dealer's point > 21.
+	# method checkBJ to see if the Dealer's point == 21.
+
 class Dealer():
 	def __init__ (self):
 		self.score = 0
 		self.hand = []
-	
 
-	def takeIn(self, card): 
+	def takeIn(self, card):
 		self.hand.append(card)
 		if card == 'A':
 			self.score += 11
-		else:	
+		else:
 			self.score += card
 
 	def checkBurst(self):
@@ -38,14 +60,21 @@ class Dealer():
 	def checkBJ(self):
 		return self.score == 21
 
-	
+# create a Player class with:
+	# attribute 'score' which keeps track of the dealer's number of points.
+	# attribute 'hand' which keeps track of the list of cards on the player's hand.
+	# attribute 'fund' which stores the amount of money the player has.
+	# method takeIn for taking in a card from the Deck.
+	# method checkBurst to see if the Dealer's point > 21.
+	# method checkBJ to see if the Dealer's point == 21.
+
 class Player():
 	def __init__ (self, fund):
 		self.score = 0
 		self.hand = []
 		self.fund = fund
 
-	def takeIn(self, card): 
+	def takeIn(self, card):
 		self.hand.append(card)
 		if card == 'A':
 			if (self.score + 11) <= 21:
@@ -53,7 +82,7 @@ class Player():
 			else:
 				self.score += 1
 		else:
-			self.score += card 
+			self.score += card
 
 	def checkBurst(self):
 		if self.score > 21 and 'A' in self.hand:
@@ -67,26 +96,26 @@ class Player():
 	def checkBJ(self):
 		return self.score == 21
 
-	
+
+# the function for the main game.
 def game():
 
 	if myPlayer.checkBJ():
 		print('You won with a BJ')
 		myPlayer.fund += bet
-		return 
+		return
 	else:
 
-
-	# Ask the player whether to hit or stand
+		# Ask the player whether to hit or stand
 
 
 		playerChoice = input('Press h for hit and s for stand: ')
 
 
 		# If player choose 'Hit', deal cards until:
-			# Player chooses 'Stand'.
-			# Player gets BJ.
-			# Player bursts.
+			# Player chooses 'Stand'
+			# or Player gets BJ.
+			# or Player bursts.
 
 		if playerChoice == 'h':
 			while playerChoice != 's' and myPlayer.checkBJ() != True and myPlayer.checkBurst() != True:
@@ -94,11 +123,11 @@ def game():
 				if myPlayer.checkBJ():
 					print('You won with a BJ')
 					myPlayer.fund += bet
-					return 
+					return
 				if myPlayer.checkBurst():
 					print('You burst. Try again')
 					myPlayer.fund -= bet
-					return 
+					return
 				print(f'Your current score is {myPlayer.score}')
 				playerChoice = input('Press H for Hit and S for Stand: ')
 				if playerChoice == 's':
@@ -108,6 +137,12 @@ def game():
 			dealerTurn()
 
 
+		# The dealer's turn comes after the player's turn.
+		# The dealer first must take in cards until its score >= 17
+		# Then:
+			# If it gets a BJ, player loses.
+			# Else if it gets a burst, player wins.
+			# Else the dealer's score and the player's score are compared, the one with the higher score wins. Equal score means a tie.
 
 def dealerTurn():
 	print(myDealer.hand)
@@ -134,44 +169,30 @@ def dealerTurn():
 
 if __name__ == '__main__':
 
-	# myDeck = Deck()
-	# print(myDeck.cardList)
-
+	# player is asked if he/she wants to start a game and how much money he/she has
 	play = 'y'
 	fund = float(input('How much money do you have? '))
 	myPlayer = Player(fund)
 
-
+	# if player agrees to start a game, a Deck is created and the player is asked for the amount of money he/she wants to bet on that game.
+	# the game loop starts
 	while play == 'y':
 		myDeck = Deck()
 		myPlayer.score = 0
 		bet = float(input('How much money do you want to bet? '))
 		myDealer = Dealer()
 
-		# Start the game by dealing cards to the dealer and the player
-
+		# Start by dealing cards to the dealer and the player
+		# Player takes in 2 cards
 		myPlayer.takeIn(myDeck.throwOut())
 		myPlayer.takeIn(myDeck.throwOut())
 		print(f'Your score is {myPlayer.score}')
-
+		# # Dealer takes in 2 cards
 		myDealer.takeIn(myDeck.throwOut())
 		myDealer.takeIn(myDeck.throwOut())
-
+		# The main game starts.
 		game()
+		# Let the player knows his/her fund after the game based on the outcome of the game.
 		print(f'You have {myPlayer.fund} in your fund')
+		# Ask player if he/she wants to play another game.
 		play = input('Do you want to keep playing? Press y for yes and n for no: ')
-
-
-
-	
-
-
-
-
-
-
-
-
-		
-
-
--- bigTicTacToe/checkOver.py
+++ bigTicTacToe/checkOver.py
@@ -1,6 +1,6 @@
-#this module helps to check if a game of TicTacToe is over
+# this module helps to check if a game of TicTacToe is over
 
-#checking if the winning streak is achieved in a row by printing out all rows as strings and see if the streak is in any row.
+# checking if the winning streak is achieved in a row by printing out all rows as strings and see if the streak is in any row.
 def checkOverRow(move_list, sign, side, streak):
 	for row in range(side):
 		checkRow = []
@@ -10,7 +10,7 @@ def checkOverRow(move_list, sign, side, streak):
 			return True
 	return False
 
-#checking if the winning streak is achieved in a row by printing out all columns as strings and see if the streak is in any column.
+# checking if the winning streak is achieved in a row by printing out all columns as strings and see if the streak is in any column.
 def checkOverCol(move_list, sign, side, streak):
 	for col in range(side):
 		checkCol = []
@@ -20,8 +20,8 @@ def checkOverCol(move_list, sign, side, streak):
 			return True
 	return False
 
-#checking if the winning streak is achieved in any negative slope diagonal by printing out all of these diagonal as strings and see if the streak is in any of them.
-#to create a list of diagonals, the function takes advantage of the fact that the (row index - column index) is the same for all cells in a diagonal.
+# checking if the winning streak is achieved in any negative slope diagonal by printing out all of these diagonal as strings and see if the streak is in any of them.
+# to create a list of diagonals, the function takes advantage of the fact that the (row index - column index) is the same for all cells in a diagonal.
 def checkOverDiagonalRight(move_list, sign, side, streak):
 	for sub in range((1 - side), side):
 		diagonal = []
@@ -33,8 +33,8 @@ def checkOverDiagonalRight(move_list, sign, side, streak):
 			return True
 	return False
 
-#checking if the winning streak is achieved in any positive slope diagonal by printing out all of these diagonal as strings and see if the streak is in any of them.
-#to create a list of diagonals, the function takes advantage of the fact that the (row index + column index) is the same for all cells in a diagonal.
+# checking if the winning streak is achieved in any positive slope diagonal by printing out all of these diagonal as strings and see if the streak is in any of them.
+# to create a list of diagonals, the function takes advantage of the fact that the (row index + column index) is the same for all cells in a diagonal.
 def checkOverDiagonalLeft(move_list, sign, side, streak):
 	for tot in range(side - 1, (2*side-2)):
 		diagonal = []
@@ -46,8 +46,8 @@ def checkOverDiagonalLeft(move_list, sign, side, streak):
 			return True
 	return False
 
-#the big checkOver function that combine the 4 smaller subfunctions above
-#this function returns True if any subfunctions finds the winning streak, otherwise it returns False to the game loop so that the game continues.
+# the big checkOver function that combine the 4 smaller subfunctions above
+# this function returns True if any subfunctions finds the winning streak, otherwise it returns False to the game loop so that the game continues.
 def checkOver(move_list,sign, side, streak):
 
 	if checkOverRow(move_list, sign, side, streak):
